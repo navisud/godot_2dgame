5d64a78896e398f2abc5951e3a0a75de11b6f047
abraham <abrahamluccas13@gmail.com>
2024-08-18 21:18:30 -0300

    hud

--- Main/main.gd
+++ Main/main.gd
@@ -3,17 +3,21 @@ extends Node
 @export var mob_scene: PackedScene
 var score
 
-func _ready():
-	new_game()
-
 func game_over():
 	$ScoreTimer.stop()
 	$MobTimer.stop()
+	$HUD.show_game_over()
 
 func new_game():
+	get_tree().call_group("mobs", "queue_free")
 	score = 0
 	$Player.start($StartPosition.position)
 	$StartTimer.start()
+	$HUD.update_score(score)
+	$HUD.show_message("Get Ready")
+
+func _on_hud_start_game():
+	pass # Replace with function body.
 
 func _on_start_timer_timeout():
 	$MobTimer.start()
@@ -21,6 +25,7 @@ func _on_start_timer_timeout():
 
 func _on_score_timer_timeout():
 	score += 1
+	$HUD.update_score(score)
 
 func _on_mob_timer_timeout():
 	# Create a new instance of the Mob scene.
